eaaec9c2aa9f25cb31ec9967e710be15dd790464
Ryder <thefifthcamel@gmail.com>
2025-12-08 15:22:56 -0800

    qf

--- Carnival/items/jokers.lua
+++ Carnival/items/jokers.lua
@@ -48,7 +48,7 @@
 SMODS.Joker{
     key = "hearts_quest",
     atlas = "atlasjokers",
-    pos = { x = 2, y = 0 },
+    pos = { x = 1, y = 0 },
     loc_txt = {
         name = "Hearts Quest",
         text = {
